0e7e457a3bc395198d985329765ab3b15f6fea09
grajkogj <67935829+grajkogj@users.noreply.github.com>
2020-09-29 14:03:03 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -9,3 +9,25 @@ function startMenu() {
         startButton.style.backgroundImage = "url('startP.png')";
     };
 };
+
+var closeButton = document.getElementById("closeWindow");
+var windowP = document.getElementById("windowP");
+var page = document.getElementById("pageFrame");
+
+function start(app) {
+    
+    var title = document.getElementById("windowTitle");
+    document.getElementById("windowP").className = "window";
+    title.innerHTML = app;
+    document.getElementById("pageFrame").src = app + ".html"
+};
+
+
+function endProcess() {
+    document.getElementById("windowP").className = "hidden";
+};
+
+
+function startSearch() {
+    console.log("started app: 'Search'")
+};
